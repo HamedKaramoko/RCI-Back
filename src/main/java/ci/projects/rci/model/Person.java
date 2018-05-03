6c75a215dd6af1ffaa21d9d3f922355d60330536
Hamed KARAMOKO <mydalmajor18@gmail.com>
2018-05-03 15:18:58 +0200

    Eager loading services in Person.

--- src/main/java/ci/projects/rci/model/Person.java
+++ src/main/java/ci/projects/rci/model/Person.java
@@ -11,6 +11,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -41,7 +42,7 @@ public class Person {
 	@Column(length=40, unique=true)
 	private String email;
 	
-	@ManyToMany(cascade={CascadeType.ALL})
+	@ManyToMany(fetch=FetchType.EAGER, cascade={CascadeType.ALL})
 	@JoinTable(name="Person_Service")
 	private Set<Service> services = new HashSet<>();
 	/**
