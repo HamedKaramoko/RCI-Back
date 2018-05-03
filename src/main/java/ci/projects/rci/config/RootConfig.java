176129efa6ebf0e02ee0ca92c90b8d8d9b72b49f
Hamed KARAMOKO <mydalmajor18@gmail.com>
2018-05-03 15:10:59 +0200

    EnableTransaction has to be in WebConfig.

--- src/main/java/ci/projects/rci/config/RootConfig.java
+++ src/main/java/ci/projects/rci/config/RootConfig.java
@@ -5,7 +5,6 @@ package ci.projects.rci.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,7 +14,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  *
  */
 @Configuration
-@EnableTransactionManagement
 @ComponentScan(basePackages="ci.projects.rci",
     excludeFilters={
         @Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)
--- src/main/java/ci/projects/rci/config/WebConfig.java
+++ src/main/java/ci/projects/rci/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -24,6 +25,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableWebMvc
 @EnableSwagger2
+@EnableTransactionManagement
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 @ComponentScan(basePackages="ci.projects.rci.business")
 public class WebConfig extends WebMvcConfigurerAdapter {
